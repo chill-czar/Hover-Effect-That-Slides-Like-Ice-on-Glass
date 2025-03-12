dd48dcdb8346c5411d65e73b34a318150f5ad285
jimmy <84112158+chill-czar@users.noreply.github.com>
2025-03-12 13:58:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,76 @@
+export const awards = [
+  {
+    name: "Indie of Year",
+    type: "Nominee",
+    project: "INNOVATE",
+    label: "Awwwards",
+  },
+  {
+    name: "Site of Day",
+    type: "Awwwards",
+    project: "LVXH-AMOT",
+    label: "See Live",
+  },
+  {
+    name: "Site of Day",
+    type: "Awwwards",
+    project: "Open Field",
+    label: "See Live",
+  },
+  {
+    name: "Best Portfolio",
+    type: "Winner",
+    project: "Henri H.",
+    label: "Awwwards",
+  },
+  {
+    name: "UI/UX Excellence",
+    type: "Finalist",
+    project: "Studio X",
+    label: "CSSDA",
+  },
+  {
+    name: "Mobile Excellence",
+    type: "Winner",
+    project: "NextGen UI",
+    label: "Awwwards",
+  },
+  {
+    name: "Best Experimental",
+    type: "Nominee",
+    project: "Web Lab",
+    label: "FWA",
+  },
+  {
+    name: "Most Innovative",
+    type: "Winner",
+    project: "Visionary",
+    label: "CSSDA",
+  },
+  {
+    name: "E-Commerce Site",
+    type: "Nominee",
+    project: "Luxury Hub",
+    label: "Awwwards",
+  },
+  {
+    name: "Best Interactive",
+    type: "Winner",
+    project: "Hyper Motion",
+    label: "Awwwards",
+  },
+];
+
+
+export const awardImages = [
+  "https://images.pexels.com/photos/31075070/pexels-photo-31075070/free-photo-of-stylish-woman-in-pink-coat-against-brick-wall.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  "https://images.pexels.com/photos/31056966/pexels-photo-31056966/free-photo-of-beautiful-cherry-blossoms-in-springtime.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/31046945/pexels-photo-31046945/free-photo-of-traditional-japanese-onsen-in-winter-snow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/28469110/pexels-photo-28469110/free-photo-of-elegant-coffee-setup-on-antique-chair-in-sunlight.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/31011991/pexels-photo-31011991/free-photo-of-dynamic-tokyo-street-scene-near-dvd-shopfront.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/30109633/pexels-photo-30109633/free-photo-of-elegant-woman-at-candlelit-dinner-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/28059307/pexels-photo-28059307/free-photo-of-a-table-set-with-candles-and-glasses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/31045891/pexels-photo-31045891/free-photo-of-elegant-portrait-of-a-woman-with-wet-look.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  "https://images.pexels.com/photos/29240782/pexels-photo-29240782/free-photo-of-stylish-man-walking-on-moroccan-coastline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/31086171/pexels-photo-31086171/free-photo-of-seagull-on-essaouira-coastline.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+];
